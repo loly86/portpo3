009460db24b3c640cb7116119ac56e6e867acc79
loly86 <chunjaymin@gmail.com>
2020-12-27 20:01:45 +0900

    first

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title>hello</v-card-title>
+    <v-card-text>
+      <v-textarea v-model="txt" />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="test">test</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      txt: '',
+    }
+  },
+  methods: {
+    async test() {
+      const r = await this.$axios.get('/api')
+      this.txt = r.data
+    },
+  },
+}
+</script>
